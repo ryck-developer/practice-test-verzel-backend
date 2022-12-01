ec583ec7eb0c43b75b079b92de460360731da39d
luiz henrique <oliveirachaves97@gmail.com>
2022-12-01 11:56:28 -0300

    adicionando restrição de dados

--- verzel/vehicle/views.py
+++ verzel/vehicle/views.py
@@ -3,20 +3,38 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from vehicle.serializers import *
 
+# ALTERAR PARA TRUE QUANDO FOR PARA PRODUÇÃO 
+# OU PERMITIR SOMENTE LEITURA NOS TESTES
+PRODUCTION_ = True
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = VehicleModel.objects.all().order_by('preco')
     serializer_class = VehicleSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    if PRODUCTION_:
+        #READ-ONLY PERMISSION
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    else:
+        # CRUD TEST API ALLOWED FOR ALL
+        permission_classes = [permissions.AllowAny]
 
 class FabricanteViewSet(viewsets.ModelViewSet):
     queryset = FabricanteModel.objects.all()
     serializer_class = FabricanteSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    if PRODUCTION_:
+        #READ-ONLY PERMISSION
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    else:
+        # CRUD TEST API ALLOWED FOR ALL
+        permission_classes = [permissions.AllowAny]
 
 class ModeloViewSet(viewsets.ModelViewSet):
     queryset = ModeloModel.objects.all()
     serializer_class = ModeloSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    if PRODUCTION_:
+        #READ-ONLY PERMISSION
+        permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    else:
+        # CRUD TEST API ALLOWED FOR ALL
+        permission_classes = [permissions.AllowAny]
 
 
